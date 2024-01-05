b3a59017beb9c023bfdfc6c7cd3f2fbd06ae3884
Atanazyy <atanazy.gawrysiak@gmail.com>
2024-01-05 15:46:36 +0100

    .

--- WDP/poprzednie/prev.cpp
+++ WDP/poprzednie/prev.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 struct wie
 {
-    int poz;
+    int poz; //indeks najdalszego elementu ktory jest w przedziale [minw, maxw] 
     long long minw, maxw;
-    shared_ptr <wie> l, p;
+    shared_ptr <wie> l, p; //wskazniki na lewego i prawego syna lub null jezeli nie istnieje
 };
 
-vector <shared_ptr <wie> > drzewa;
-vector <long long> tab;
+vector <shared_ptr <wie> > drzewa; //wskazniki na korzenie drzew poszczegolnych elementow
+vector <long long> tab; //wartosci w tablicy 
 
-void dodaj(shared_ptr <wie> v, shared_ptr <wie> odp)
+void dodaj(shared_ptr <wie> v, shared_ptr <wie> odp) //tworzy wierzcholek drzewa
 {
-    if(v->minw == v->maxw)
+    if(v->minw == v->maxw) //jestesmy w najnizszym mozliwym lisciu
         return;
     long long sr = (v->minw + v->maxw) / 2;
-    if(tab[v->poz] <= sr)
+    if(tab[v->poz] <= sr) //trzeba utworzyc lewego syna
     {
         if(odp != NULL)
             v->p = odp->p;
@@ -33,7 +33,7 @@ void dodaj(shared_ptr <wie> v, shared_ptr <wie> odp)
         else
             dodaj(nowy, NULL);
     }
-    else
+    else //trzeba utworzyc prawego syna
     {
         if(odp != NULL)
             v->l = odp->l;
@@ -50,11 +50,11 @@ void dodaj(shared_ptr <wie> v, shared_ptr <wie> odp)
     }
 }
 
-void pushBack(int x)
+void pushBack(int x) //dodaje element na koniec ciagu
 {
     shared_ptr <wie> v = make_shared <wie>();
     drzewa.push_back(v);
-    tab.push_back((long long)x + INT_MAX);
+    tab.push_back((long long)x + INT_MAX); //trzymanie ujemnych liczb sprawia problemy wiec przesowam wszystko o INT_MAX
     v->poz = (int)tab.size() - 1;
     v->minw = 0;
     v->maxw = 3 * (long long)INT_MAX;
@@ -62,20 +62,18 @@ void pushBack(int x)
     dodaj(v, drzewa[drzewa.size() - 2]);
 }
 
-int szukaj(shared_ptr <wie> v, long long lo, long long hi)
+int szukaj(shared_ptr <wie> v, long long lo, long long hi) //zwraca indeks najdalszego elementu ktory jest w przedziale [lo,hi] o ktorym informacja jest w poddrzewie o korzeniu v
 {
     if(v == NULL)
         return -1;
-    //cout << v->minw - INT_MAX << " " << v->maxw - INT_MAX << " " << v-> poz << " " << v->l << " " << v->p << "\n" << flush;
     if(v->minw >= lo && v->maxw <= hi)
         return v->poz - 1;
     if(v->minw > hi || v->maxw < lo)
         return -1;
     return max(szukaj(v->l, lo, hi), szukaj(v->p, lo, hi));
-    //cout << "\n";
 }
 
-int prevInRange(int i, int lo, int hi)
+int prevInRange(int i, int lo, int hi) //zwraca najwiekszy indeks nie wiekszy niz i takiego elemntu ktory jest w przedziale [lo, hi]
 {
     return szukaj(drzewa[i + 1], (long long)lo + INT_MAX, (long long)hi + INT_MAX);
 }
@@ -90,5 +88,7 @@ void init(const vector<int> &vec)
 
 void done()
 {
+    drzewa.clear();
+    tab.clear();
     return;
 }
